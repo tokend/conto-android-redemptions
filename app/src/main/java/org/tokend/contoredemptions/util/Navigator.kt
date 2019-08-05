9585008e67be33c7246ffa9dd48ee16747cb04e3
Oleg Koretsky <test.radiokot@gmail.com>
2019-08-05 15:03:16 +0300

    Add navigator component

---
+++ app/src/main/java/org/tokend/contoredemptions/util/Navigator.kt
@@ -0,0 +1,94 @@
+package org.tokend.contoredemptions.util
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityOptionsCompat
+import androidx.fragment.app.Fragment
+import org.jetbrains.anko.newTask
+import org.tokend.contoredemptions.R
+
+/**
+ * Performs transitions between screens.
+ * 'open-' will open related screen as a child.<p>
+ * 'to-' will open related screen and finish current.
+ */
+class Navigator private constructor() {
+    private var activity: Activity? = null
+    private var fragment: Fragment? = null
+    private var context: Context? = null
+
+    companion object {
+        fun from(activity: Activity): Navigator {
+            val navigator = Navigator()
+            navigator.activity = activity
+            navigator.context = activity
+            return navigator
+        }
+
+        fun from(fragment: Fragment): Navigator {
+            val navigator = Navigator()
+            navigator.fragment = fragment
+            navigator.context = fragment.requireContext()
+            return navigator
+        }
+
+        fun from(context: Context): Navigator {
+            val navigator = Navigator()
+            navigator.context = context
+            return navigator
+        }
+    }
+
+    private fun performIntent(intent: Intent?, requestCode: Int? = null, bundle: Bundle? = null) {
+        if (intent != null) {
+            if (!IntentLock.checkIntent(intent, context)) return
+            activity?.let {
+                if (requestCode != null) {
+                    it.startActivityForResult(intent, requestCode, bundle ?: Bundle.EMPTY)
+                } else {
+                    it.startActivity(intent, bundle ?: Bundle.EMPTY)
+                }
+                return
+            }
+
+            fragment?.let {
+                if (requestCode != null) {
+                    it.startActivityForResult(intent, requestCode, bundle ?: Bundle.EMPTY)
+                } else {
+                    it.startActivity(intent, bundle ?: Bundle.EMPTY)
+                }
+                return
+            }
+
+            context?.startActivity(intent.newTask(), bundle ?: Bundle.EMPTY)
+        }
+    }
+
+    private fun fadeOut(activity: Activity) {
+        ActivityCompat.finishAfterTransition(activity)
+        activity.overridePendingTransition(0, R.anim.activity_fade_out)
+        activity.finish()
+    }
+
+    private fun createTransitionBundle(activity: Activity, vararg pairs: Pair<View?, String>): Bundle {
+        val sharedViews = arrayListOf<androidx.core.util.Pair<View, String>>()
+
+        pairs.forEach {
+            val view = it.first
+            if (view != null) {
+                sharedViews.add(androidx.core.util.Pair(view, it.second))
+            }
+        }
+
+        return if (sharedViews.isEmpty()) {
+            Bundle.EMPTY
+        } else {
+            ActivityOptionsCompat.makeSceneTransitionAnimation(activity,
+                    *sharedViews.toTypedArray()).toBundle() ?: Bundle.EMPTY
+        }
+    }
+}
